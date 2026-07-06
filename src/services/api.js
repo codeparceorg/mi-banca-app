e12fc813cc3d4672479a7edc45cca02b1ad14082
codeparce <codeparcelts@gmail.com>
2026-07-06 12:13:10 -0500

    Devops: update: Actualizacion de ingress

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_GATEWAY || 'http://localhost:8080/api';
+const API_BASE_URL = import.meta.env.VITE_API_GATEWAY || 'http://microservicio.sis-main-ms.lab';
 
 export const api = {
   async register(name, email, password) {
