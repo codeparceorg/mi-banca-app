52e7ab516da1f3b948b4741e63924e04b3952c11
codeparce <codeparcelts@gmail.com>
2026-07-22 11:47:41 -0500

    Devops: New: Implementacion de argo rollout

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  }
 })
